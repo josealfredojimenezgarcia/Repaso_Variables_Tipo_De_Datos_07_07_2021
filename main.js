a59884fbae890fedd5c1985b57166740df7bb70c
josealfredojimenezgarcia <joseajimenez07@gmail.com>
2021-07-07 20:57:52 -0500

    Repaso_Tipo_Variable_ 07_07_2021

---
+++ main.js
@@ -0,0 +1,36 @@
+
+
+nombreCompletoDelProfesor = `Miguel Angel Castro Escamilla :V`;
+miEdad = 23;
+// altura = 1.43;
+soyUnProfesor = true;
+a='Si lo puedes pensar, lo puedes realizar; solo falta tú voluntad'
+b=56
+c="Soy Ganador Siempre"
+d= true
+e=false
+f= 65.357
+g={'a':'ab'}
+h=('a',2, 'b', 3)
+let i=[4,5,'a']
+
+console.log(nombreCompletoDelProfesor, typeof(nombreCompletoDelProfesor));
+console.log(miEdad, typeof(miEdad));
+// console.log(altura, typeof(altura));
+console.log(soyUnProfesor, typeof(soyUnProfesor));
+
+console.log(a,b,c);
+
+console.log(`Juega Limpio ${a}`);
+console.log(`Variable a : ${typeof (a)}`);
+console.log(`Variable c : ${typeof (c)}`);
+console.log(`Variable d : ${typeof (d)}`);
+console.log(`Variable e : ${typeof (e)}`);
+console.log(`Variable f : ${typeof (f)}`);
+console.log(`Variable g : ${typeof (g)}`);
+console.log(`Variable h : ${typeof (h)}`);
+console.log(i);
+
+
+// alert()
+// document.write();
